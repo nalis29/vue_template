b154b87b16bb7f7bfcdf9fa18ec742c67645a4cf
Nalis Un <nalis.un@bi-kay.com>
2023-07-12 22:25:54 +0700

    init template

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="text-primary">
+    <h1>This is a very basic homeview</h1>
+    <p>This is the content</p>
+  </main>
+</template>
+
+
+<style lang="scss">
+
+  main {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+
+</style>
